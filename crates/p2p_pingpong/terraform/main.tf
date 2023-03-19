e88b05b9672e8be41b6f22bf9d25515735209cc8
sgdxbc <sung@comp.nus.edu.sg>
2023-03-19 11:26:52 +0000

    pingpong: Cross region cluster setup

---
+++ crates/p2p_pingpong/terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "ap-east-1"
+  region = "ap-east-1"
+}
+
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
+
+module "region-1" {
+  source = "./region"
+  providers = {
+    aws = aws.ap-east-1
+  }
+}
+
+module "region-2" {
+  source = "./region"
+  providers = {
+    aws = aws.ap-southeast-1
+  }
+}
+
+output "address-1" {
+  value = module.region-1.address
+}
+
+output "address-2" {
+  value = module.region-2.address
+}
--- crates/p2p_pingpong/terraform/region/main.tf
+++ crates/p2p_pingpong/terraform/region/main.tf
@@ -7,34 +7,21 @@ terraform {
   }
 }
 
-provider "aws" {
-  alias = "ap-east-1"
-  region = "ap-east-1"
-}
-
 resource "aws_vpc" "esys-pingpong" {
-  provider = aws.ap-east-1
-
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_subnet" "esys-pingpong" {
-  provider = aws.ap-east-1
-
-  vpc_id = resource.aws_vpc.esys-pingpong.id
-  cidr_block = "10.0.0.0/16"
+  vpc_id                  = resource.aws_vpc.esys-pingpong.id
+  cidr_block              = "10.0.0.0/16"
   map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "esys-pingpong" {
-  provider = aws.ap-east-1
-
   vpc_id = resource.aws_vpc.esys-pingpong.id
 }
 
 resource "aws_route_table" "esys-pingpong" {
-  provider = aws.ap-east-1
-
   vpc_id = resource.aws_vpc.esys-pingpong.id
 
   route {
@@ -44,15 +31,11 @@ resource "aws_route_table" "esys-pingpong" {
 }
 
 resource "aws_route_table_association" "_1" {
-  provider = aws.ap-east-1
-
   route_table_id = resource.aws_route_table.esys-pingpong.id
-  subnet_id = resource.aws_subnet.esys-pingpong.id
+  subnet_id      = resource.aws_subnet.esys-pingpong.id
 }
 
 resource "aws_security_group" "esys-pingpong" {
-  provider = aws.ap-east-1
-
   vpc_id = resource.aws_vpc.esys-pingpong.id
 
   ingress {
@@ -73,8 +56,6 @@ resource "aws_security_group" "esys-pingpong" {
 }
 
 data "aws_ami" "ubuntu" {
-  provider = aws.ap-east-1
-
   most_recent = true
 
   filter {
@@ -91,16 +72,13 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "esys-pingpong" {
-  provider = aws.ap-east-1
-  count = 2
-
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  subnet_id = resource.aws_subnet.esys-pingpong.id
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = resource.aws_subnet.esys-pingpong.id
   vpc_security_group_ids = [resource.aws_security_group.esys-pingpong.id]
-  key_name = "Ephemeral"
+  key_name               = "Ephemeral"
 }
 
 output "address" {
-  value = resource.aws_instance.esys-pingpong[*].public_ip
+  value = resource.aws_instance.esys-pingpong.public_ip
 }
