e173178cdbff01128858bf5cd1c4613f1701ad13
sgdxbc <sung@comp.nus.edu.sg>
2023-04-17 14:24:42 +0000

    Add automate scripts for coding microbench

--- crates/esys_wirehair/examples/coding_bench.rs
+++ crates/esys_wirehair/examples/coding_bench.rs
@@ -3,17 +3,16 @@ use std::{collections::HashMap, time::Instant};
 use esys_wirehair::{WirehairDecoder, WirehairEncoder};
 use rand::{random, thread_rng, RngCore};
 
+
 const OBJECT_SIZE: usize = 1 << 30;
 fn main() {
     let outer_codings = [(4, 5), (8, 10), (12, 15)];
     let inner_codings = [(32, 80)];
 
-    println!("chunk_k,chunk_n,fragment_k,fragment_n,encode,decode");
+    println!("chunk_k,chunk_n,fragment_k,fragment_n,op,time");
     for (chunk_k, chunk_n) in outer_codings {
         for (fragment_k, fragment_n) in inner_codings {
-            for _ in 0..3 {
-                run(chunk_k, chunk_n, fragment_k, fragment_n);
-            }
+            run(chunk_k, chunk_n, fragment_k, fragment_n);
         }
     }
 }
@@ -42,6 +41,13 @@ fn run(chunk_k: usize, chunk_n: usize, fragment_k: usize, fragment_n: usize) {
     }
 
     let encode = Instant::now() - start;
+    println!(
+        "{chunk_k},{chunk_n},{fragment_k},{fragment_n},encode,{}",
+        encode.as_secs_f32()
+    );
+
+    let repair_chunk = chunks.values().next().unwrap().clone();
+
     let start = Instant::now();
 
     let mut outer_decoder = WirehairDecoder::new(OBJECT_SIZE as _, (OBJECT_SIZE / chunk_k) as _);
@@ -67,8 +73,29 @@ fn run(chunk_k: usize, chunk_n: usize, fragment_k: usize, fragment_n: usize) {
 
     let decode = Instant::now() - start;
     println!(
-        "{chunk_k},{chunk_n},{fragment_k},{fragment_n},{},{}",
-        encode.as_secs_f32(),
+        "{chunk_k},{chunk_n},{fragment_k},{fragment_n},decode,{}",
         decode.as_secs_f32()
+    );
+
+    let start = Instant::now();
+    let mut repair_decoder = WirehairDecoder::new(
+        (OBJECT_SIZE / chunk_k) as _,
+        (OBJECT_SIZE / chunk_k / fragment_k) as _,
+    );
+    for (fragment_id, fragment) in repair_chunk {
+        if repair_decoder.decode(fragment_id, &fragment).unwrap() {
+            break;
+        }
+    }
+    let mut repaired_fragment = vec![0; OBJECT_SIZE / chunk_k / fragment_k];
+    repair_decoder
+        .into_encoder()
+        .unwrap()
+        .encode(random(), &mut repaired_fragment)
+        .unwrap();
+    let repair = Instant::now() - start;
+    println!(
+        "{chunk_k},{chunk_n},{fragment_k},{fragment_n},repair,{}",
+        repair.as_secs_f32()
     );
 }
